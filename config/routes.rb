3c58bee194669152abab04accad5b74c741f688c
Chris Blunt <chris@plymouthsoftware.com>
2020-05-30 14:10:51 +0100

    Allow users to apply for jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :job_applications
-  resources :job_posts
+  resources :job_posts do
+    resources :job_applications
+  end
 
   root to: redirect("/job_posts")
 end
