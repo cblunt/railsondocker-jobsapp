3b70ba8c45bd70ca74eafab85e7dbef898c4ca93
Chris Blunt <chris@plymouthsoftware.com>
2020-05-30 14:01:39 +0100

    Add name fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_124853) do
+ActiveRecord::Schema.define(version: 2020_05_30_125707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,10 @@ ActiveRecord::Schema.define(version: 2020_05_30_124853) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "admin"
+    t.index ["admin"], name: "index_users_on_admin"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
