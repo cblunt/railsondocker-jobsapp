efa35c2e076b37d2789f7ef572d29c183232d712
Chris Blunt <chris@plymouthsoftware.com>
2020-05-30 22:34:15 +0100

    Create factories for specs

---
+++ test/factories/job_posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :job_post do
+    title { 'A Job Post' }
+    body { 'Work for us at our company.' }
+  end
+end
---
+++ test/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    first_name { 'A' }
+    last_name { 'User' }
+    email { 'user@example.com' }
+    admin { false }
+    password { 'secret123' }
+
+    trait :admin do
+      first_name { 'An' }
+      last_name { 'Admin' }
+      admin { true }
+      email { 'admin@example.com' }
+    end
+  end
+end
