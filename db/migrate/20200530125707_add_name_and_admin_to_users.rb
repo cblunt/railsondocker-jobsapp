7a4d0d32276040dfc9e5359b5d4fef640bfd27e1
Chris Blunt <chris@plymouthsoftware.com>
2020-05-30 14:01:39 +0100

    Add name fields to users

---
+++ db/migrate/20200530125707_add_name_and_admin_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameAndAdminToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :admin, :boolean
+    add_index :users, :admin
+  end
+end
