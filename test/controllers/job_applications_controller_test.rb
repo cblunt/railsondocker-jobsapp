82fc2a406060e1b94c522633172dd1fd296ea05f
Chris Blunt <chris@plymouthsoftware.com>
2020-05-30 22:39:41 +0100

    Job Applications test passing

--- test/controllers/job_applications_controller_test.rb
+++ test/controllers/job_applications_controller_test.rb
@@ -1,48 +1,57 @@
 require 'test_helper'
 
 class JobApplicationsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @job_application = job_applications(:one)
-  end
+  let(:job_post) { FactoryBot.create(:job_post) }
 
-  test "should get index" do
-    get job_applications_url
-    assert_response :success
-  end
+  describe 'as an admin' do
+    let(:admin) { FactoryBot.create(:user, :admin) }
+    let(:job_application) { FactoryBot.create(:job_application, job_post: job_post) }
 
-  test "should get new" do
-    get new_job_application_url
-    assert_response :success
-  end
-
-  test "should create job_application" do
-    assert_difference('JobApplication.count') do
-      post job_applications_url, params: { job_application: { body: @job_application.body, job_post_id: @job_application.job_post_id, user_id: @job_application.user_id } }
+    test 'should get index' do
+      get job_post_job_applications_url(as: admin, job_post_id: job_post.id)
+      assert_response :success
     end
 
-    assert_redirected_to job_application_url(JobApplication.last)
-  end
-
-  test "should show job_application" do
-    get job_application_url(@job_application)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_job_application_url(@job_application)
-    assert_response :success
-  end
-
-  test "should update job_application" do
-    patch job_application_url(@job_application), params: { job_application: { body: @job_application.body, job_post_id: @job_application.job_post_id, user_id: @job_application.user_id } }
-    assert_redirected_to job_application_url(@job_application)
-  end
-
-  test "should destroy job_application" do
-    assert_difference('JobApplication.count', -1) do
-      delete job_application_url(@job_application)
+    test 'should show job_application' do
+      get job_post_job_application_url(as: admin, job_post_id: job_post.id, id: job_application.id)
+      assert_response :success
     end
 
-    assert_redirected_to job_applications_url
+    test 'should get edit' do
+      get edit_job_post_job_application_url(as: admin, job_post_id: job_post.id, id: job_application.id)
+      assert_response :success
+    end
+
+    test 'should update job_application' do
+      patch job_post_job_application_url(as: admin, job_post_id: job_post.id, id: job_application.id), params: { job_application: { body: 'Updated application' } }
+      assert_redirected_to job_post_url(job_post)
+    end
+
+    test 'should destroy job_application' do
+      job_application # We need to create the job application for assert_difference to work
+
+      assert_difference('JobApplication.count', -1) do
+        delete job_post_job_application_url(as: admin, job_post_id: job_post.id, id: job_application.id)
+      end
+
+      assert_redirected_to job_post_url(job_post)
+    end
+  end
+
+  describe 'as a user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    test 'should get new' do
+      get new_job_post_job_application_url(as: user, job_post_id: job_post.id)
+      assert_response :success
+    end
+
+    test 'should create job_application' do
+      assert_difference('JobApplication.count') do
+        post job_post_job_applications_url(as: user, job_post_id: job_post.id), params: { job_application: { body: 'I am applying' } }
+      end
+
+      assert_redirected_to job_post_url(job_post)
+    end
   end
 end
