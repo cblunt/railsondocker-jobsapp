941e08b44ae60c32dfea9572e70444e1c3b19932
Chris Blunt <chris@plymouthsoftware.com>
2021-12-29 20:05:21 +0000

    Add model associations

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,2 +1,7 @@
 class JobPost < ApplicationRecord
+  has_many :job_applications
+  has_many :applicants, through: :job_applications
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
