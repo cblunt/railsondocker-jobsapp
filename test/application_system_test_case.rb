e08cfe30f36a82e3b5ec04288ac1a3aa71860424
Chris Blunt <chris@plymouthsoftware.com>
2022-02-06 19:58:14 +0000

    Configure system tests to use selenium grid

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,35 @@
 require "test_helper"
 
+Capybara.register_driver :chrome_headless do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions': {
+      args: %w[headless no-sandbox disable-gpu disable-dev-shm-usage]
+    }
+  )
+
+  if ENV["HUB_URL"]
+    Capybara::Selenium::Driver.new(app,
+      browser: :remote,
+      url: ENV["HUB_URL"],
+      capabilities: capabilities,
+    )
+  else
+    # Fall back to default
+    Capybara::Selenium::Driver.new(app,
+                                  browser: :chrome,
+                                  capabilities: capabilities)
+  end
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :chrome_headless
+
+  def setup
+    # Fetch the IP address of the container running the specs.
+    ip_address = Socket.ip_address_list.select { |addr| addr.ipv4_private? && !addr.ipv4_loopback? }.first.ip_address
+
+    Capybara.app_host = "http://#{ip_address}"
+    Capybara.server_host = "0.0.0.0"
+    Capybara.always_include_port = true
+  end
 end
--- test/system/job_posts_test.rb
+++ test/system/job_posts_test.rb
@@ -1,43 +1,18 @@
 require "application_system_test_case"
 
 class JobPostsTest < ApplicationSystemTestCase
-  setup do
-    @job_post = job_posts(:one)
-  end
+  let(:admin) { FactoryBot.create(:user, :admin) }
 
-  test "visiting the index" do
-    visit job_posts_url
-    assert_selector "h1", text: "Job posts"
-  end
+  test 'creating a Job Post' do
+    visit job_posts_url(as: admin)
 
-  test "should create job post" do
-    visit job_posts_url
-    click_on "New job post"
+    click_on 'New job post'
 
-    fill_in "Body", with: @job_post.body
-    fill_in "Title", with: @job_post.title
-    click_on "Create Job post"
+    fill_in 'Title', with: 'A Job Post'
+    fill_in 'Body', with: 'Work for us at our company.'
 
-    assert_text "Job post was successfully created"
-    click_on "Back"
-  end
+    click_on 'Create Job post'
 
-  test "should update Job post" do
-    visit job_post_url(@job_post)
-    click_on "Edit this job post", match: :first
-
-    fill_in "Body", with: @job_post.body
-    fill_in "Title", with: @job_post.title
-    click_on "Update Job post"
-
-    assert_text "Job post was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Job post" do
-    visit job_post_url(@job_post)
-    click_on "Destroy this job post", match: :first
-
-    assert_text "Job post was successfully destroyed"
+    expect(text).must_match(/Job post was successfully created/)
   end
 end
