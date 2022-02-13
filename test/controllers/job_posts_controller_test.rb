731e61a41fe43444bfe6cc3289a11b4833c6c39a
Chris Blunt <chris@plymouthsoftware.com>
2022-02-13 19:49:18 +0000

    Passing tests

--- test/controllers/job_posts_controller_test.rb
+++ test/controllers/job_posts_controller_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class JobPostsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @job_post = job_posts(:one)
-  end
+  let(:job_post) { FactoryBot.create(:job_post) }
+
+  let(:admin) { FactoryBot.create(:user, :admin) }
 
   test "should get index" do
     get job_posts_url
@@ -11,36 +11,38 @@ class JobPostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_job_post_url
+    get new_job_post_url(as: admin)
     assert_response :success
   end
 
   test "should create job_post" do
     assert_difference("JobPost.count") do
-      post job_posts_url, params: { job_post: { body: @job_post.body, title: @job_post.title } }
+      post job_posts_url(as: admin), params: { job_post: { body: "A job post", title: "A job Post" } }
     end
 
     assert_redirected_to job_post_url(JobPost.last)
   end
 
   test "should show job_post" do
-    get job_post_url(@job_post)
+    get job_post_url(job_post)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_job_post_url(@job_post)
+    get edit_job_post_url(job_post, as: admin)
     assert_response :success
   end
 
   test "should update job_post" do
-    patch job_post_url(@job_post), params: { job_post: { body: @job_post.body, title: @job_post.title } }
-    assert_redirected_to job_post_url(@job_post)
+    patch job_post_url(job_post, as: admin), params: { job_post: { body: job_post.body, title: job_post.title } }
+    assert_redirected_to job_post_url(job_post)
   end
 
   test "should destroy job_post" do
+    job_post
+
     assert_difference("JobPost.count", -1) do
-      delete job_post_url(@job_post)
+      delete job_post_url(job_post, as: admin)
     end
 
     assert_redirected_to job_posts_url
--- test/system/job_applications_test.rb
+++ test/system/job_applications_test.rb
@@ -1,45 +1,51 @@
 require "application_system_test_case"
 
 class JobApplicationsTest < ApplicationSystemTestCase
-  setup do
-    @job_application = job_applications(:one)
-  end
+  let(:applicant) { FactoryBot.create(:user) }
+  let(:admin) { FactoryBot.create(:user, :admin) }
+
+  let(:job_application) { FactoryBot.create(:job_application) }
+  let(:job_post) { FactoryBot.create(:job_post) }
 
   test "visiting the index" do
-    visit job_applications_url
+    visit job_post_job_applications_url(job_post, as: admin)
+
     assert_selector "h1", text: "Job applications"
   end
 
   test "should create job application" do
-    visit job_applications_url
-    click_on "New job application"
+    visit job_post_url(job_post, as: applicant)
 
-    fill_in "Body", with: @job_application.body
-    fill_in "Job post", with: @job_application.job_post_id
-    fill_in "User", with: @job_application.user_id
+    click_on "Apply Now"
+
+    fill_in "Body", with: "I would like to apply for this job"
+    attach_file "Cv", "test/fixtures/files/sample.txt"
+
     click_on "Create Job application"
 
-    assert_text "Job application was successfully created"
+    expect(text).must_match("Job application was successfully created")
+
     click_on "Back"
   end
 
   test "should update Job application" do
-    visit job_application_url(@job_application)
-    click_on "Edit this job application", match: :first
+    visit job_post_job_application_url(job_application.job_post, job_application, as: admin)
 
-    fill_in "Body", with: @job_application.body
-    fill_in "Job post", with: @job_application.job_post_id
-    fill_in "User", with: @job_application.user_id
+    click_on "Edit this job application", match: :first
+    fill_in "Body", with: job_application.body
+    attach_file "Cv", "test/fixtures/files/sample.txt"
     click_on "Update Job application"
 
-    assert_text "Job application was successfully updated"
+    expect(text).must_match("Job application was successfully updated")
+
     click_on "Back"
   end
 
   test "should destroy Job application" do
-    visit job_application_url(@job_application)
+    visit job_post_job_application_url(job_application.job_post, job_application, as: admin)
+
     click_on "Destroy this job application", match: :first
 
-    assert_text "Job application was successfully destroyed"
+    expect(text).must_match("Job application was successfully destroyed")
   end
 end
